13f7b7e6e54077d752605d356fbbf7618ab0dc21
sliterok <korobatov2011@yandex.ru>
2016-09-26 23:03:53 +0500

    Bit refactory

--- voteit.js
+++ voteit.js
@@ -2,24 +2,23 @@
 var request = require('request');
 //!!!
 var proxyfile = 'proxylist.txt';
-var url = 'https://telegram-club.ru/like/1669';
+var url = 'http://telegram-club.ru/like/1669';
 //!!!
 fs.readFile(proxyfile, function(err, data) {
     if(err) throw err;
-    var array = data.toString().split("\n");
-    for(i=0; i<array.length; i++) {
-		console.log(array[i]);
+    array = data.toString().split("\n");
+	for(var i = 0; i<array.length; i++)
+	{
 		post('http://'+array[i]);
-    }
+	}
 });
 function post(proxy){
 	request.get({uri: url, headers: {'X-Requested-With': 'XMLHttpRequest'}, proxy: proxy},
 		function optionalCallback(err, httpResponse, body) {
 		  if (err) {
 			  console.log(err);
-			return;
 		  }
-		  if(true){console.log('Worked!'+proxy);}
+		  else{console.log('Worked!'+body+proxy);}
 		}
 	);
 }
